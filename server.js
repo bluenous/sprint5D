d670a87bebc9313b9cc611f0a39ff13c6969e818
Danny Gómez <styven.gomez@udea.edu.co>
2021-10-22 17:58:26 -0500

    Jserverjasper

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("./db.json");
@@ -14,9 +15,8 @@ server.use(
 server.use(router);
 server.listen(PORT, () => {
   console.log("Server is running");
-});
+});*/
 
-/*
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
@@ -26,4 +26,4 @@ const port = process.env.PORT || 5000;
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);*/
+server.listen(port);
